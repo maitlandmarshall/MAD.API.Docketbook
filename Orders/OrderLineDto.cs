87846e6032cb8651ef3d045c33f92723fe7fdf44
maitlandmarshall <maitlandmarshallapps@gmail.com>
2022-01-20 08:14:49 +1000

    Add Resource and Uom to OrderLineDto for creating order lines with Resources

--- Orders/OrderLineDto.cs
+++ Orders/OrderLineDto.cs
@@ -12,5 +12,18 @@ namespace MAD.API.Docketbook.Orders
         public bool? Delete { get; set; }
 
         public string SupplierRef { get; set; }
+
+        public Guid? Resource { get; set; }
+        public Guid? ResourceCategory { get; set; }
+
+        public UomDto Uom { get; set; }
+
+        public class UomDto
+        {
+            public Guid Id { get; set; }
+            public string Abbreviation { get; set; }
+            public string Description { get; set; }
+            public Guid Organisation { get; set; }
+        }
     }
 }
