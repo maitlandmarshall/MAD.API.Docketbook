639d6f16a05b9b49fa390c5c86f2d7128db16235
maitlandmarshall <maitlandmarshallapps@gmail.com>
2022-06-09 12:46:19 +1000

    Ensure the DocketIngestDto Date is formatted as yyyy-MM-dd

--- MAD.API.Docketbook/Dockets/DocketIngestDto.cs
+++ MAD.API.Docketbook/Dockets/DocketIngestDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,7 @@ namespace MAD.API.Docketbook.Dockets
 {
     public class DocketIngestDto
     {
+        [JsonConverter(typeof(DocketIngestDtoDateTimeConverter))]
         public DateTime Date { get; set; }
         public string Operator { get; set; }
         public string DocketNumber { get; set; }
---
+++ MAD.API.Docketbook/Dockets/DocketIngestDtoDateTimeConverter.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace MAD.API.Docketbook.Dockets
+{
+    internal class DocketIngestDtoDateTimeConverter : IsoDateTimeConverter
+    {
+        public DocketIngestDtoDateTimeConverter()
+        {
+            this.DateTimeFormat = "yyyy-MM-dd";
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            base.WriteJson(writer, value, serializer);
+        }
+    }
+}
